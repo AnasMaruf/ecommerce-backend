2bb1709de8d0802ed1deedf43bd3cdeb7ed07b15
AnasMaruf <anasmaruf1101@gmail.com>
2024-10-31 22:07:03 +0700

    Add handle callback midtrans

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\MidtransController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/midtrans/callback', [MidtransController::class, 'callback']);
 
 // Route::get('/testing', function () {
 //     $response = \Http::withHeaders(
