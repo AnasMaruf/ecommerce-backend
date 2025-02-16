a4602fd2199860ed319538396e099d797e2df857
AnasMaruf <anasmaruf1101@gmail.com>
2025-02-16 15:07:10 +0700

    Add ShouldQueue on all email

--- app/Mail/SendRegisterOTP.php
+++ app/Mail/SendRegisterOTP.php
@@ -4,13 +4,13 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendRegisterOTP extends Mailable
+class SendRegisterOTP extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
     public $user;
